2236a4a2175692d0ca87e1eaa0f10340b0c776d0
hxy7yx <1595670487@qq.com>
2025-02-24 22:31:38 -0800

    tmp main

--- cross-build.sh
+++ cross-build.sh
@@ -105,5 +105,5 @@ function compile_source_with_tag() {
 
 sudo rm -rf $neuron_dir/*
 mkdir -p $neuron_dir
-compile_source_with_tag $user neuron $branch
-compile_source_with_tag $user neuron-modules $branch
+compile_source_with_tag emqx neuron main
+compile_source_with_tag emqx neuron-modules main
